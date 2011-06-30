8d014a3007fc6529952f7429a23ec8d9b2b1c6cb
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-06-29 21:11:29 -0400

    Packet encoding; AAAA support

--- dns/dns.rkt
+++ dns/dns.rkt
@@ -2,11 +2,13 @@
 
 (require (planet tonyg/bitsyntax))
 (require racket/udp)
+(require racket/match)
 
 (require "mapping.rkt")
 
 ;; Protocol data taken from RFC-1035. (See also RFC-1034.)
 ;; Blocks of text inside <rfc1035>...</rfc1035> also from RFC-1035.
+;; RFC-3596 specifies "DNS Extensions to Support IP Version 6".
 
 ;;---------------------------------------------------------------------------
 ;; Structure definitions
@@ -75,7 +77,8 @@
   (hinfo 13)
   (minfo 14)
   (mx 15)
-  (txt 16))
+  (txt 16)
+  (aaaa 28))
 
 (define-mapping qtype->value value->qtype
   #:forward-default type->value
@@ -185,24 +188,25 @@
 		    additional-section)))))
 
 (define (dns-message->packet m)
-  (bit-string ((dns-message-id m) : bits 16)
-	      ((boolean->bit (dns-message-response? m)) : bits 1)
-	      ((query-opcode->value (dns-message-opcode m)) : bits 4)
-	      ((boolean->bit (dns-message-authoritative? m)) : bits 1)
-	      ((boolean->bit (dns-message-truncated? m)) : bits 1)
-	      ((boolean->bit (dns-message-recursion-desired? m)) : bits 1)
-	      ((boolean->bit (dns-message-recursion-available? m)) : bits 1)
-	      (0 : bits 3)
-	      ((query-response-code->value (dns-message-response-code m)) : bits 4)
-	      ((length (dns-message-questions m)) : bits 16)
-	      ((length (dns-message-answers m)) : bits 16)
-	      ((length (dns-message-authorities m)) : bits 16)
-	      ((length (dns-message-additional m)) : bits 16)
-	      ((bit-string-append
-		(encode-section encode-question (dns-message-questions m))
-		(encode-section encode-rr (dns-message-answers m))
-		(encode-section encode-rr (dns-message-authorities m))
-		(encode-section encode-rr (dns-message-additional m))) : binary)))
+  (bit-string->bytes
+   (bit-string ((dns-message-id m) : bits 16)
+	       ((boolean->bit (dns-message-response? m)) : bits 1)
+	       ((query-opcode->value (dns-message-opcode m)) : bits 4)
+	       ((boolean->bit (dns-message-authoritative? m)) : bits 1)
+	       ((boolean->bit (dns-message-truncated? m)) : bits 1)
+	       ((boolean->bit (dns-message-recursion-desired? m)) : bits 1)
+	       ((boolean->bit (dns-message-recursion-available? m)) : bits 1)
+	       (0 : bits 3)
+	       ((query-response-code->value (dns-message-response-code m)) : bits 4)
+	       ((length (dns-message-questions m)) : bits 16)
+	       ((length (dns-message-answers m)) : bits 16)
+	       ((length (dns-message-authorities m)) : bits 16)
+	       ((length (dns-message-additional m)) : bits 16)
+	       ((bit-string-append
+		 (encode-section encode-question (dns-message-questions m))
+		 (encode-section encode-rr (dns-message-answers m))
+		 (encode-section encode-rr (dns-message-authorities m))
+		 (encode-section encode-rr (dns-message-additional m))) : binary))))
 
 (define (parse-section packet parser remaining-records input)
   (let loop ((count remaining-records)
@@ -258,6 +262,22 @@
 	(cons name (extract-domain-names whole-packet remainder)))
       '()))
 
+(define (encode-domain-name labels)
+  (cond
+   ((null? labels) (bytes))
+   ((null? (cdr labels)) (encode-label (car labels)))
+   (else (bit-string-append (encode-label (car labels))
+			    (encode-domain-name (cdr labels))))))
+
+(define (encode-label label)
+  (encode-pascal-string "Label" 64 label))
+
+(define (encode-pascal-string string-kind length-limit s)
+  (let ((len (bytes-length s)))
+    (when (>= len length-limit)
+      (error 'encode-pascal-string "~s too long: ~v" string-kind s))
+    (bytes-append (bytes len) s)))
+
 ;; Character strings are pascal-style length-byte-prefixed strings.
 
 (define (extract-character-strings input)
@@ -267,6 +287,9 @@
     ([len (body : binary bytes len) (rest : binary)]
      (cons (bit-string->bytes body)
 	   (extract-character-strings rest)))))
+
+(define (encode-character-string bs)
+  (encode-pascal-string "Character-string" 256 bs))
 
 ;; <rfc1035>
 ;; The question section is used to carry the "question" in most queries,
@@ -298,7 +321,9 @@
 	       tail)))))
 
 (define (encode-question q)
-  (error 'bang))
+  (bit-string-append (encode-domain-name (question-name q))
+		     (bit-string ((qtype->value (question-type q)) : bits 16)
+				 ((qclass->value (question-class q)) : bits 16))))
 
 ;; <rfc1035>
 ;; All RRs have the same top level format shown below:
@@ -351,8 +376,8 @@
 	    ([(preference : bits 16) (exchange : binary)]
 	     (mx preference (parse-single-domain-name whole-packet exchange)))))
     ((null) (bit-string->bytes rdata))
-    ((soa) (let*-values (((mname rdata1) (parse-domain-name whole-packet rdata))
-			 ((rname rdata2) (parse-domain-name whole-packet rdata1)))
+    ((soa) (let*-values (((mname rdata1) (parse-domain-name whole-packet rdata '()))
+			 ((rname rdata2) (parse-domain-name whole-packet rdata1 '())))
 	     (bit-string-case rdata2
 	       ([(serial : bits 32)
 		 (refresh : bits 32)
@@ -364,13 +389,51 @@
     ((a) (bit-string-case rdata
 	   ([a b c d]
 	    (vector a b c d))))
+    ((aaaa) (bit-string-case rdata
+	      ([(ipv6-addr : binary bits 128)]
+	       (list->vector (bytes->list (bit-string->bytes ipv6-addr))))))
     ((wks) (bit-string-case rdata
 	     ([a b c d protocol (bitmap : binary)]
 	      (wks (vector a b c d) protocol bitmap))))
     (else (bit-string->bytes rdata))))
 
 (define (encode-rr rr)
-  (error 'bang))
+  (let ((encoded-rdata (encode-rdata (rr-type rr) (rr-rdata rr))))
+    (bit-string-append (encode-domain-name (rr-name rr))
+		       (bit-string ((type->value (rr-type rr)) : bits 16)
+				   ((class->value (rr-class rr)) : bits 16)
+				   ((rr-ttl rr) : bits 32)
+				   ((/ (bit-string-length encoded-rdata) 8) : bits 16)
+				   (encoded-rdata : binary)))))
+
+(define (encode-rdata type rdata)
+  (case type
+    ((cname mb md mf mg mr ns ptr) (encode-domain-name rdata))
+    ((hinfo) (bit-string-append (encode-character-string (hinfo-cpu rdata))
+				(encode-character-string (hinfo-os rdata))))
+    ((minfo) (bit-string-append (encode-character-string (minfo-rmailbx rdata))
+				(encode-character-string (minfo-emailbx rdata))))
+    ((mx) (bit-string ((mx-preference rdata) : bits 16)
+		      ((encode-domain-name (mx-exchange rdata)) : binary)))
+    ((null) rdata)
+    ((soa) (bit-string-append (encode-domain-name (soa-mname rdata))
+			      (encode-domain-name (soa-rname rdata))
+			      (bit-string ((soa-serial rdata) : bits 32)
+					  ((soa-refresh rdata) : bits 32)
+					  ((soa-retry rdata) : bits 32)
+					  ((soa-expire rdata) : bits 32)
+					  ((soa-minimum rdata) : bits 32))))
+    ((txt)
+     ;; TODO: write and use bit-string-append* instead of using apply here
+     (foldl (lambda (s acc) (bit-string-append acc (encode-character-string s)))
+	    (car rdata)
+	    (cdr rdata)))
+    ((a) (match rdata ((vector a b c d) (bit-string a b c d))))
+    ((aaaa) (bit-string ((list->bytes (vector->list rdata)) : binary bits 128)))
+    ((wks) (match (wks-address rdata)
+	     ((vector a b c d)
+	      (bit-string a b c d (wks-protocol rdata) ((wks-bitmap rdata) : binary)))))
+    (else rdata)))
 
 ;;---------------------------------------------------------------------------
 
@@ -474,3 +537,89 @@
 (require racket/pretty)
 (pretty-print (packet->dns-message (q-google-in-any)))
 (pretty-print (packet->dns-message (a-google-in-any)))
+
+(pretty-print (dns-message->packet (packet->dns-message (a-google-in-any))))
+
+;; Wed Jun 29 20:59:17 2011 (4e0bca65): UDP: localhost sent 28 bytes:
+;; 00000000: 47 16 01 00 00 01 00 00 : 00 00 00 00 06 67 6F 6F  G............goo
+;; 00000010: 67 6C 65 03 63 6F 6D 00 : 00 1C 00 01              gle.com.....
+;; 0000001C:
+;; Wed Jun 29 20:59:17 2011 (4e0bca65): UDP: pass through succeeded
+;; Wed Jun 29 20:59:17 2011 (4e0bca65): UDP: google-public-dns-a.google.com sent 78 bytes:
+;; 00000000: 47 16 81 80 00 01 00 00 : 00 01 00 00 06 67 6F 6F  G............goo
+;; 00000010: 67 6C 65 03 63 6F 6D 00 : 00 1C 00 01 C0 0C 00 06  gle.com.........
+;; 00000020: 00 01 00 00 02 52 00 26 : 03 6E 73 31 C0 0C 09 64  .....R.&.ns1...d
+;; 00000030: 6E 73 2D 61 64 6D 69 6E : C0 0C 00 16 33 23 00 00  ns-admin....3#..
+;; 00000040: 1C 20 00 00 07 08 00 12 : 75 00 00 00 01 2C        . ......u....,
+;; 0000004E:
+
+(pretty-print
+ (packet->dns-message
+  (bytes
+   #x47 #x16 #x01 #x00 #x00 #x01 #x00 #x00 #x00 #x00 #x00 #x00 #x06 #x67 #x6F #x6F
+   #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00 #x01)))
+
+(pretty-print
+ (packet->dns-message
+  (bytes
+   #x47 #x16 #x81 #x80 #x00 #x01 #x00 #x00 #x00 #x01 #x00 #x00 #x06 #x67 #x6F #x6F
+   #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00 #x01 #xC0 #x0C #x00 #x06
+   #x00 #x01 #x00 #x00 #x02 #x52 #x00 #x26 #x03 #x6E #x73 #x31 #xC0 #x0C #x09 #x64
+   #x6E #x73 #x2D #x61 #x64 #x6D #x69 #x6E #xC0 #x0C #x00 #x16 #x33 #x23 #x00 #x00
+   #x1C #x20 #x00 #x00 #x07 #x08 #x00 #x12 #x75 #x00 #x00 #x00 #x01 #x2C)))
+
+;; Wed Jun 29 21:05:03 2011 (4e0bcbbf): UDP: localhost sent 32 bytes:
+;; 00000000: 12 70 01 00 00 01 00 00 : 00 00 00 00 03 77 77 77  .p...........www
+;; 00000010: 06 67 6F 6F 67 6C 65 03 : 63 6F 6D 00 00 1C 00 01  .google.com.....
+;; 00000020:
+;; Wed Jun 29 21:05:03 2011 (4e0bcbbf): UDP: pass through succeeded
+;; Wed Jun 29 21:05:03 2011 (4e0bcbbf): UDP: ns1.google.com sent 52 bytes:
+;; 00000000: 12 70 85 00 00 01 00 01 : 00 00 00 00 03 77 77 77  .p...........www
+;; 00000010: 06 67 6F 6F 67 6C 65 03 : 63 6F 6D 00 00 1C 00 01  .google.com.....
+;; 00000020: C0 0C 00 05 00 01 00 09 : 3A 80 00 08 03 77 77 77  ........:....www
+;; 00000030: 01 6C C0 10             :                          .l..
+;; 00000034:
+
+(pretty-print
+ (packet->dns-message
+  (bytes
+   #x12 #x70 #x01 #x00 #x00 #x01 #x00 #x00 #x00 #x00 #x00 #x00 #x03 #x77 #x77 #x77
+   #x06 #x67 #x6F #x6F #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00 #x01)))
+
+(pretty-print
+ (packet->dns-message
+  (bytes
+   #x12 #x70 #x85 #x00 #x00 #x01 #x00 #x01 #x00 #x00 #x00 #x00 #x03 #x77 #x77 #x77
+   #x06 #x67 #x6F #x6F #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00 #x01
+   #xC0 #x0C #x00 #x05 #x00 #x01 #x00 #x09 #x3A #x80 #x00 #x08 #x03 #x77 #x77 #x77
+   #x01 #x6C #xC0 #x10)))
+
+;; Wed Jun 29 21:07:46 2011 (4e0bcc62): UDP: ns1.google.com sent 82 bytes:
+;; 00000000: 23 79 85 00 00 01 00 02 : 00 00 00 00 04 69 70 76  #y...........ipv
+;; 00000010: 36 06 67 6F 6F 67 6C 65 : 03 63 6F 6D 00 00 1C 00  6.google.com....
+;; 00000020: 01 C0 0C 00 05 00 01 00 : 09 3A 80 00 09 04 69 70  .........:....ip
+;; 00000030: 76 36 01 6C C0 11 C0 2D : 00 1C 00 01 00 00 01 2C  v6.l...-.......,
+;; 00000040: 00 10 20 01 48 60 80 0F : 00 00 00 00 00 00 00 00  .. .H`..........
+;; 00000050: 00 68                   :                          .h
+;; 00000052:
+
+(pretty-print
+ (packet->dns-message
+  (bytes
+   #x23 #x79 #x85 #x00 #x00 #x01 #x00 #x02 #x00 #x00 #x00 #x00 #x04 #x69 #x70 #x76
+   #x36 #x06 #x67 #x6F #x6F #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00
+   #x01 #xC0 #x0C #x00 #x05 #x00 #x01 #x00 #x09 #x3A #x80 #x00 #x09 #x04 #x69 #x70
+   #x76 #x36 #x01 #x6C #xC0 #x11 #xC0 #x2D #x00 #x1C #x00 #x01 #x00 #x00 #x01 #x2C
+   #x00 #x10 #x20 #x01 #x48 #x60 #x80 #x0F #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x00
+   #x00 #x68)))
+
+(pretty-print
+ (dns-message->packet
+  (packet->dns-message
+   (bytes
+    #x23 #x79 #x85 #x00 #x00 #x01 #x00 #x02 #x00 #x00 #x00 #x00 #x04 #x69 #x70 #x76
+    #x36 #x06 #x67 #x6F #x6F #x67 #x6C #x65 #x03 #x63 #x6F #x6D #x00 #x00 #x1C #x00
+    #x01 #xC0 #x0C #x00 #x05 #x00 #x01 #x00 #x09 #x3A #x80 #x00 #x09 #x04 #x69 #x70
+    #x76 #x36 #x01 #x6C #xC0 #x11 #xC0 #x2D #x00 #x1C #x00 #x01 #x00 #x00 #x01 #x2C
+    #x00 #x10 #x20 #x01 #x48 #x60 #x80 #x0F #x00 #x00 #x00 #x00 #x00 #x00 #x00 #x00
+    #x00 #x68))))
